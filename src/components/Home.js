6fb5f39bfa5ed66f271b2e7e03e6f245c0a3776c
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-01-23 12:09:30 -0800

    Readme.md file added & edited title of site to Course Directory in index & on home pg

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
   render() {
     return (
       <div className="main-content home">
-        <h2>Front End Course Directory</h2>
+        <h2>Course Directory</h2>
         <p>This fun directory is a project using <em>React Router</em>.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales sapien viverra, rutrum leo quis, varius enim. Mauris hendrerit pulvinar sem, et mollis leo tristique et. Aenean a velit laoreet, commodo sapien et, mattis lorem. Fusce id mi quis felis porttitor convallis vitae bibendum tellus. Maecenas ac lectus eu turpis tincidunt sollicitudin.</p>
         <p>Pellentesque vitae porttitor sem. Aliquam erat sapien, lobortis ut quam eu, pulvinar finibus nisl. Suspendisse potenti. Ut eget iaculis ex, vitae egestas ligula. Sed ultricies nibh non ipsum posuere finibus.</p>
